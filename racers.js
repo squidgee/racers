476d5f9394188d4c702b82041ce15f4985a83966
Squidgee <114772982+squidgee@users.noreply.github.com>
2025-04-18 14:40:51 +0100

    Add files via upload

--- racers.js
+++ racers.js
@@ -3,7 +3,7 @@
 */
 
 // don't change me - required to not produce errors in console
-version = 17042025;
+version = 18042025;
 racersInRace = [];
 racerOutcomesLOSERS = [];
 racerOutcomesWINNERS = [];
